2797daab4c2078a077d37eb91ea6f7b03adc800c
Aleksandrov Semyon <aleksandrosemnik@gmail.com>
2020-09-26 14:10:24 +0300

    feat: all homework complete

--- calculator/script.js
+++ calculator/script.js
@@ -7,11 +7,13 @@ const allClearButton = document.querySelector("[data-all-clear]");
 const prevOperandTextElem = document.querySelector("[data-previous-operand]");
 const currOperandTextElem = document.querySelector("[data-current-operand]");
 const equalsButton = document.querySelector("[data-equals]");
+const plusMinusButton = document.querySelector("[data-plus-minus]");
 
 class Calculator {
   constructor(prevOperandTextElem, currOperandTextElem) {
     this.prevOperandTextElem = prevOperandTextElem;
     this.currOperandTextElem = currOperandTextElem;
+    this.readyToReset = false; // Переменная для сброса результата
     this.clearDisplay();
   }
 
@@ -19,15 +21,32 @@ class Calculator {
     this.prevOperand = "";
     this.currOperand = "";
     this.operation = undefined;
+    this.readyToReset = false;
   }
 
   delete() {
-    this.currOperand = this.currOperand.slice(0, -1); //toString()
+    this.currOperand = this.currOperand.slice(0, -1);
   }
 
   addNumber(number) {
     if (number === "." && this.currOperand.includes(".")) return;
     this.currOperand = this.currOperand.toString() + number.toString();
+  }
+
+  sqrtNumber() {
+    if (this.operation === "√") {
+      this.readyToReset = true;
+      if (this.prevOperand < 0) return (this.currOperand = "Error");
+      // двойное приведение типов к Числу
+      return (this.currOperand = +Number(Math.sqrt(this.prevOperand)).toFixed(
+        6
+      ));
+    }
+  }
+
+  negateNumber() {
+    if (this.currOperand > 0) return (this.currOperand = this.currOperand * -1);
+    if (this.currOperand < 0) return (this.currOperand = this.currOperand * -1);
   }
 
   chooseOperation(operation) {
@@ -38,12 +57,7 @@ class Calculator {
     this.operation = operation;
     this.prevOperand = this.currOperand;
     this.currOperand = "";
-
-    if (operation === "x2") {
-      return (this.currOperand = Math.pow(this.prevOperand, 2));
-    } else if (operation === "√") {
-      return (this.currOperand = Math.abs(Math.sqrt(this.prevOperand)));
-    }
+    this.sqrtNumber();
   }
 
   calculateResult() {
@@ -53,20 +67,25 @@ class Calculator {
     if (isNaN(prev) || isNaN(curr)) return;
     switch (this.operation) {
       case "+":
-        result = (prev + curr).toFixed(1);
+        result = prev + curr;
         break;
       case "-":
         result = prev - curr;
         break;
       case "x":
-        result = (prev * curr).toFixed(1);
+        result = prev * curr;
         break;
       case "÷":
         result = prev / curr;
         break;
+      case "xy":
+        result = Math.pow(prev, curr);
+        break;
       default:
         return;
     }
+    result = +Number(result).toFixed(6);
+    this.readyToReset = true;
     this.currOperand = result;
     this.prevOperand = "";
     this.operation = undefined;
@@ -74,10 +93,11 @@ class Calculator {
 
   updateDisplay() {
     this.currOperandTextElem.innerText = this.currOperand;
+
     if (this.operation === "√") {
       this.prevOperandTextElem.innerText = `${this.operation} (${this.prevOperand})`;
-    } else if (this.operation === "x2") {
-      this.prevOperandTextElem.innerText = `sqrt(${this.prevOperand})`;
+    } else if (this.operation === "xy") {
+      this.prevOperandTextElem.innerText = `${this.prevOperand} ^`;
     } else if (this.operation) {
       this.prevOperandTextElem.innerText = `${this.prevOperand} ${this.operation}`;
     } else {
@@ -90,7 +110,24 @@ const calculator = new Calculator(prevOperandTextElem, currOperandTextElem);
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
+    if (
+      (calculator.prevOperand !== "" || calculator.prevOperand === "") &&
+      calculator.currOperand !== "" &&
+      calculator.readyToReset
+    ) {
+      calculator.operation = undefined;
+      calculator.prevOperand = "";
+      calculator.currOperand = "";
+      calculator.readyToReset = false;
+    }
     calculator.addNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
     calculator.updateDisplay();
   });
 });
@@ -105,14 +142,12 @@ deleteButton.addEventListener("click", () => {
   calculator.updateDisplay();
 });
 
-operationButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    calculator.chooseOperation(button.innerText);
-    calculator.updateDisplay();
-  });
-});
-
 equalsButton.addEventListener("click", () => {
   calculator.calculateResult();
+  calculator.updateDisplay();
+});
+
+plusMinusButton.addEventListener("click", () => {
+  calculator.negateNumber();
   calculator.updateDisplay();
 });
